57d3462a2cba3b7dc4f9b7aaa3a8611d2dab1161
akushwah1987 <aakash.kushwah1987@outlook.com>
2025-03-22 22:56:41 +0530

    initialized 3 services

---
+++ acctmgmt-service/src/main/java/com/archexc/acctmgmtservice/AcctmgmtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.archexc.acctmgmtservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcctmgmtServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcctmgmtServiceApplication.class, args);
+    }
+
+}
